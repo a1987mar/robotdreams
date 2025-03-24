e69db36a006ce5494f3e014399c4485f1a0a8ede
Andriy <a1987mar@gmail.com>
2025-03-25 00:42:13 +0200

    main.go

--- cms/main.go
+++ cms/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"robotlesson/config"
 	"robotlesson/pkg"
@@ -9,14 +10,18 @@ import (
 func main() {
 	config := config.LoadConfig()
 	http.Handle("GET /login", Login())
+	fmt.Println("Start server")
 	server := http.Server{
 		Addr: config.Serv.Port,
 	}
-	server.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Stop server")
+	}
 }
 
 func Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		pkg.JsonResponse(w, "HelloWorld", http.StatusOK)
+		pkg.JsonResponse(w, "Hello World", http.StatusOK)
 	}
 }
---
+++ internal/login/handler.go
@@ -0,0 +1,7 @@
+package login
+
+type Login struct {
+	Email    string `json:"emeil"`
+	Passwors string `json:"password"`
+	Name     string `json:"name"`
+}
