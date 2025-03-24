0abf893b334ce92c8715347037fccad3a66c2048
Andriy <a1987mar@gmail.com>
2025-03-24 23:47:55 +0200

    main.go

--- cms/main.go
+++ cms/main.go
@@ -8,9 +8,17 @@ import (
 func main() {
 	config := config.LoadConfig()
 
+	http.HandleFunc("POST /login", Login())
+
 	server := http.Server{
 		Addr: config.Serv.Port,
 	}
 	server.ListenAndServe()
 
+}
+
+func Login() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	}
 }
